0241baf62203bf573b579b923794aa8fbc326d3d
Ope Adeyomoye <victoradeyomoye@yahoo.co.uk>
2017-06-20 08:48:59 +0100

    📚 Plans for developer interaction with the library

--- src/Cyclos/Cyclos.php
+++ src/Cyclos/Cyclos.php
@@ -1,13 +1,23 @@
 <?php
- 
- namespace Cyclos;
 
- class Cyclos
- {
+namespace Cyclos;
 
- }
-
+class Cyclos
+{
+    public function getJar()
+    {
+        if ($this->james != $this->kate) {
+            if (5 >= $this->kate) {
+                return false;
+            }
+        }
+        return array('key' => 'value');
+    }
+}
  /**
-  * Models ==> 
-  *
+  * Models ==> objects representing request datasets
+           ==> objects representing response types, formats, datasets
+  * 
+  * Requests => endpoint url sections, request method, 
+  * Client and configuration
   */
