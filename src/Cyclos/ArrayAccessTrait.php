25df2881753becb153b4bda621ce715383aed9cb
Ope Adeyomoye <victoradeyomoye@yahoo.co.uk>
2017-06-22 19:50:19 +0100

    ✨ Added trait implementation of \ArrrayAccess.

---
+++ src/Cyclos/ArrayAccessTrait.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Cyclos;
+
+/**
+ * An implementation of the \ArrayAccess interface.
+ * 
+ * Based (almost-entirely) off of https://bitbucket.org/dgifford/array-access-trait/
+ */
+trait ArrayAccessTrait
+{
+    protected $container = [];
+
+    public function offsetSet($index, $properties)
+    {
+        if (is_null($index)) {
+            $this->container[] = $properties;
+        } else {
+            $this->container[$index] = $properties;
+        }
+    }
+
+    public function offsetExists($index)
+    {
+        return isset($this->container[$index]);
+    }
+
+    public function offsetUnset($index)
+    {
+        if (isset($this->container[$index])) {
+            unset($this->container[$index]);
+        }
+    }
+
+    public function offsetGet($index)
+    {
+        if (isset($this->container[$index])) {
+            return $this->container[$index];
+        }
+        return null;
+    }
+    
+    public function patchIn($data)
+    {
+        return array_merge($this->container, $data);
+    }
+}
