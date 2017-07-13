e39dd0a8e5c903a39d47e48a44c21279620bc331
Ope Adeyomoye <victoradeyomoye@yahoo.co.uk>
2017-07-13 01:48:52 +0100

    🔖 So you see, at this point, everything works (well, you know..)

--- src/Cyclos/Http/Clients/BaseClient.php
+++ src/Cyclos/Http/Clients/BaseClient.php
@@ -2,9 +2,11 @@
 
 namespace Cyclos\Http\Clients;
 
+use Cyclos\Configuration\Configuration;
+use Cyclos\Http\ClientInterface;
 use Cyclos\Operation;
 use Cyclos\OperationAwareTrait;
-use Cyclos\Http\ClientInterface;
+use Cyclos\Response\Response;
 
 // @todo be sure to explicity set user agent for requests
 abstract class BaseClient implements ClientInterface
@@ -24,6 +26,12 @@ abstract class BaseClient implements ClientInterface
     public function setOperation(Operation $operation)
     {
         $this->operation = $operation;
+        return $this;
+    }
+
+    public function setConfig(Configuration $config)
+    {
+        $this->operation->setConfig($config);
         return $this;
     }
 
@@ -62,5 +70,21 @@ abstract class BaseClient implements ClientInterface
     {
         $this->body = $data;
         return $this;
+    }
+
+
+    /**
+     * Undocumented function
+     *
+     * @param mixed $model
+     * @param Operation $operation
+     * @param int $code
+     * @param array $headers
+     * @param stdClass $body
+     * @return void
+     */
+    public function makeResponse($model = null, Operation $operation, $code, array $headers, $body)
+    {
+        return Response::make($model, $operation, $code, $headers, $body);
     }
 }
--- src/Cyclos/Http/Clients/Httpful.php
+++ src/Cyclos/Http/Clients/Httpful.php
@@ -19,6 +19,7 @@ class Httpful extends BaseClient
     public function __construct()
     {
         // first things first...
+        // hmm... is this really necessary? Especially after saying "use Httpful\Httpful" ?
         if (!class_exists('Httpful\Httpful')) {
             throw new \BadMethodCallException(
                 'Could not find Httpful client library. Please install it, or use another
@@ -61,20 +62,57 @@ class Httpful extends BaseClient
         is_array($headers) || $headers = [];
         $headers = array_merge($headers, self::USER_AGENT_HEADER);
 
-        $this->request->addHeaders($headers);
-
         // set up body. For now...
         if ($this->body) {
             $body = $this->body;
             (is_string($body) && json_decode($body)) || $body = json_encode($body);
-            $this->request->body($this->body)->expects('json');
+            $this->request->body($this->body);
         }
 
+        // add authorization header
+        $config = $this->operation->getConfig();
+
+        if ($config->getAccessClient()) {
+            $headers['Access-Client'] = $config->getAccessClient();
+        } elseif ($config->getSessionToken()) {
+            $headers['Session-Token'] = $config->getSessionToken();
+        } else {
+            $username = $config->getUsername();
+            $password = $config->getPassword();
+
+            if (!$username || !$password) {
+                throw new \Exception('Invalid or unset authorization for Cyclos request.');
+            } else {
+                $headers['Authorization'] = 'Basic ' . base64_encode("{$username}:{$password}");
+            }
+        }
+
+        // send and model response...?
+        $this->request->addHeaders($headers);
+        $this->request->expects('json');
     }
 
+
+    /**
+     * Send the current request.
+     * 
+     * Models the response after a specified object, if present.
+     *
+     * @return mixed
+     */
     public function send()
     {
         $this->initRequest();
         $this->setupRequest();
+
+        $response = ($this->request->send());
+
+        return $this->makeResponse(
+            $this->expectedModel,
+            $this->operation,
+            $response->code,
+            $response->headers->toArray(),
+            $response->body
+        );
     }
 }
