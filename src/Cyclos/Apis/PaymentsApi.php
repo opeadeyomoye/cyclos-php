d1367317da0dfc821a6bc90fec78071f3626e6c4
Ope Adeyomoye <victoradeyomoye@yahoo.co.uk>
2017-09-15 05:41:20 +0100

    🚧 Adding payments api collection..

---
+++ src/Cyclos/Apis/PaymentsApi.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: opeadeyomoye
+ * Date: 9/15/17
+ * Time: 5:36 AM
+ */
+
+namespace Cyclos\Apis;
+
+
+use Cyclos\Configuration\ConfigAwareTrait;
+use Cyclos\Http\ClientAwareTrait;
+use Cyclos\OperationAwareTrait;
+
+class PaymentsApi
+{
+    use ConfigAwareTrait;
+    use ClientAwareTrait;
+    use OperationAwareTrait;
+
+
+}
